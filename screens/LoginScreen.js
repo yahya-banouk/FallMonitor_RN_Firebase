a231574adfc3269a3759c1757aa2b288f77c76bc
yahya-banouk <yahya.banouk@uit.ac.ma>
2022-10-20 00:29:42 +0200

    logUI

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, KeyboardAvoidingView } from 'react-native'
+import { View, Text,  StyleSheet,KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 const LoginScreen = () => {
@@ -6,8 +6,41 @@ const LoginScreen = () => {
     <KeyboardAvoidingView
       style={styles.container}
       behavior= "padding" >
-        <View>
+        <View style={styles.inputContainer}>
+        <TextInput placeholder='Email'
+            //value={}
+            //onChangeText={text => }
+            style = {styles.input}/>
+          <TextInput placeholder='Email'
+            //value={}
+            //onChangeText={text => }
+            style = {styles.input}
+            secureTextEntry/>
+
           
+        </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+          onPress={()=>{
+
+          }}
+          style={styles.button}
+          >
+            <Text style={styles.button}>Login</Text>
+            
+
+          </TouchableOpacity>
+          <TouchableOpacity
+          onPress={()=>{
+
+          }}
+          style={[styles.buttonText, styles.buttonOutline]}
+          >
+            <Text style={styles.buttonOutlineText}>Regiter</Text>
+            
+
+          </TouchableOpacity>
+
         </View>
 
 
@@ -16,5 +49,45 @@ const LoginScreen = () => {
     
   )
 }
+
+const styles = StyleSheet.create(
+  {
+    container:
+    {
+      flex : 1,
+      justifyConttent: 'center',
+      alignItems : 'center',
+      
+
+    },
+    input:
+    {
+      width: '80%'
+
+    },
+    buttonContainer:
+    {
+
+    },
+    button:
+    {
+
+    },
+    buttonOutline :
+    {
+
+    },
+    buttonOutlineText :
+    {
+
+    },
+    buttonText:
+    {
+      
+    }
+
+    
+  }
+)
 
 export default LoginScreen
